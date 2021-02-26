a98ffb1200f21ab07225c57d46254ab2afed00b4
Angat Yogi <angat.yogi@ou.edu>
2021-02-25 20:35:18 -0600

    Add project files.

---
+++ SubString/MethodCall.cs
@@ -0,0 +1,52 @@
+﻿
+
+namespace SubString
+{
+    public class MethodCall
+    {
+        //ask for user input
+
+
+        //make method that can be called in Main Method in Program class
+        public string getFirstBetweenBraces(string userInput)
+        {
+
+            //check if the string contains both braces '{' and '}'
+
+            if (userInput.Contains("{") == true && userInput.Contains("}") == true)
+            {
+                //find the index of first '{'
+
+                int indexFirstLeftCurlyBrace = userInput.IndexOf("{");
+                //find the index of first '}'
+                int indexFirstRightCurlyBrace = userInput.IndexOf("}");
+
+                //if there is nothing in between the braces.
+                if ((indexFirstRightCurlyBrace - indexFirstLeftCurlyBrace) == 1)
+                {
+
+                    return "There is nothing in between the braces";
+                }
+                else
+                {
+                    string subStringBetweenBraces = userInput.Substring((indexFirstLeftCurlyBrace + 1), (indexFirstRightCurlyBrace - (indexFirstLeftCurlyBrace + 1)));
+
+                    return subStringBetweenBraces;
+                }
+
+            }
+
+            //if the string does not contain either one or both of the braces then return empty string
+            else
+            {
+
+                return " ";
+
+            }
+
+
+        }
+
+    }
+}
+
---
+++ SubString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SubString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask for user input
+            Console.WriteLine("Please input a string.");
+
+            string userInput = Console.ReadLine();
+
+
+            //Create a object of MethodCall Class
+
+            MethodCall methodCall = new MethodCall();
+
+            //call method from MethodCall class and store in a Variable
+            string subString = methodCall.getFirstBetweenBraces(userInput);
+
+            //display the returned string to the console
+            Console.WriteLine(subString);
+
+        }
+    }
+}
